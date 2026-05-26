483ee8066c2c32ef909c1e998f466e7306260830
Sojo C Johny <sojocjohny37@gmail.com>
2026-05-26 17:30:31 +0530

    Implemented authentication using neon js service.

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,15 +1,37 @@
 // import { Dumbbell } from 'lucide-react'
 import { Link } from 'react-router-dom'
+import Button from '../ui/Button'
+import { useAuth } from '../../context/authContext'
 
 const Navbar = () => {
+  const { user } = useAuth();
+
   return (
     <header className='fixed top-0 left-0 right-0 border-b border-border bg-background/80 backdrop-blur-md'>
       <div className='max-w-6xl mx-auto px-6 h-16 flex items-center justify-between'>
         <Link to="/" className='flex items-center gap-2 text-foreground'>
           {/* <Dumbbell className='w-6 h-6 text-accent'/> */}
-          <img className='w-6 h-6' src="https://img.icons8.com/ios-filled/100/a3e635/dumbbell.png" alt="dumbbell"/>
+          <img className='w-6 h-6' src="https://img.icons8.com/ios-filled/100/a3e635/dumbbell.png" alt="dumbbell" />
           <span>SmartRep</span>
         </Link>
+        <nav>
+          {user ? (
+            <>
+            <Link to="/profile">
+                <Button size='sm' variant='ghost'>My Plan</Button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link to="/auth/sign-in">
+                <Button size='sm' variant='ghost'>Sign In</Button>
+              </Link>
+              <Link to="/auth/sign-up">
+                <Button size='sm'>Sign Up</Button>
+              </Link>
+            </>
+          )}
+        </nav>
       </div>
     </header>
   )
--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { AuthView } from '@neondatabase/neon-js/auth/react';
+import { useParams } from 'react-router-dom';
 
 const Auth = () => {
+  const { pathname } = useParams();
+
   return (
-    <div>Auth</div>
+    <div className='min-h-screen pt-24 pb-12 px-6 flex items-center justify-center'>
+      <div className='max-w-md w-full'>
+        <AuthView pathname={pathname || ''} />
+      </div>
+    </div>
   )
 }
 
