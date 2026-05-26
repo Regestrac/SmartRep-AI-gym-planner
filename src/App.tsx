ab1107712a34cade325b5992fa84bcfa5781e0e1
Sojo C Johny <sojocjohny37@gmail.com>
2026-05-26 15:42:13 +0530

    Created base components, added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Onboarding from "./pages/Onboarding"
+import Profile from "./pages/Profile"
+import Account from "./pages/Account"
+import Auth from "./pages/Auth"
+import Navbar from "./components/layout/Navbar"
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <div className="main-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1">
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/onboarding" element={<Onboarding />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/auth/:pathname" element={<Auth />} />
+          <Route path="/account/:pathname" element={<Account />} />
+        </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   )
 }
 
