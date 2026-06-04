9710c185a5405a326536cb5c3f9fd9b7423aa8ad
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-04 18:03:48 +0530

    Updated prisma config

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -2,7 +2,26 @@ import 'dotenv/config'
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
 
-const connectionString = process.env.DATABASE_URL!;
+function createPrisma() {
+  const connectionString = process.env.DATABASE_URL;
+  if (!connectionString) {
+    throw new Error("DATABASE_URL is not set in environment variables");
+  }
+  const adapter = new PrismaPg({ connectionString });
+  return new PrismaClient({ adapter });
+}
 
-const adapter = new PrismaPg({ connectionString });
-export const prisma = new PrismaClient({ adapter });
+let _prisma: PrismaClient | null = null;
+
+export function getPrisma(): PrismaClient {
+  if (!_prisma) {
+    _prisma = createPrisma();
+  }
+  return _prisma;
+}
+
+export const prisma = new Proxy({} as PrismaClient, {
+  get(_, prop) {
+    return getPrisma()[prop as keyof PrismaClient];
+  },
+});
